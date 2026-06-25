5c5cf55ede0bfbc99266e4e05809e51bef2f7e0a
Provash <provash20cb@gmail.com>
2026-06-25 14:37:57 +0600

    21-8 Optimizing The Auth Middleware With Higher Order Function

--- src/utlis/jwt.ts
+++ src/utlis/jwt.ts
@@ -12,10 +12,18 @@ const createToken=(payload: JwtPayload, secret: string, expiresIn : SignOptions)
 const verifyToken = (token :string, secret: string)=>{
     try {
         const verifiedToken =  jwt.verify(token, secret);
-    return verifiedToken;
+    // return verifiedToken;
+    return {
+        success :true,
+        data: verifiedToken
+    }
     } catch (error : any) {
         console.log("Token Verification failed:", error)
-        throw new Error (error.message);
+        // throw new Error (error.message);
+        return {
+            success:false,
+            error:error.message
+        }
     }
 }
 
